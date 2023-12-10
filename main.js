403c44cfc4849b065d16ff440021171503bfd5ef
hareemzaf <150966998+hareemzaf@users.noreply.github.com>
2023-12-10 05:20:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// for ( let i = 0; i < 10; i++){
+//     console.log("hello world");
+// }
+// for (let i = 0; i <6; i++){
+//     console.log("hello world");
+// }
+// for (let i = 0; i <11; i++){
+//     console.log("hello world");
+// }
+// for(let i = 1; i < 11; i++) {
+//     console.log(i);
+// }
+var std = ["Hamzah", "arham", "usama"];
+for (var i = 0; i < 3; i++) {
+    console.log(std[0]);
+}
---
+++ main.ts
@@ -0,0 +1,103 @@
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+// for ( let i = 0; i < 10; i++){
+//     console.log("hello world");
+// }
+
+
+
+// for (let i = 0; i <6; i++){
+//     console.log("hello world");
+// }
+
+
+// for (let i = 0; i <11; i++){
+//     console.log("hello world");
+// }
+
+
+// for(let i = 1; i < 11; i++) {
+//     console.log(i);
+// }
+
+
+// const std = ["Hamzah", "arham", "usama"];
+// for (let i = 0; i < 3; i++ ) {
+//     console.log( std[0] );
+
+
+
+
+// First time  parent loop
+
+
+
+// let videos = ["video1", "video2", "video3", "video4", "video5"];
+
+// // console.log(videos.length) // 5
+
+// for  (let i = 0; i < videos.length; i++){
+//     console.log(videos[i])
+// }
+
+// let video1Title = "video1"
+// let video1Description = "video 1 description"
+// let video1Image = "https://image.com"
+
+// let video1 = {
+//     title: "video1",
+//     description: "video 1 description",
+//     image: "https://image.com"
+// }
+
+// const videos = [
+//     {
+//         title: "video1",
+//         description: "video 1 description",
+//         image: "https://image.com"
+//     },
+//     {
+//         title: "video2",
+//         description: "video 2 description",
+//         image: "https://image.com"
+//     },
+// ]
+
+// for (let i = 0; i < videos.length; i++) {
+//     console.log(videos[i].description )
+// }
+
+// for(let i=0; i<2; i++){
+//     console.log(videos[i].title)
+// }
+
+// for (let i = 0; i< videos.length; i++){
+//     console.log(videos[i].title)
+// }
+
+
+// let video1 = {
+//     title: "video1",
+//     description: "video 1 description",
+//     image: "https://image.com",
+//     views: 1000,
+
+//     log: () => { console.log("hello") }
+// }
+
+
+// video1.log()
+
+// console.log()
+
+
+
+
+
+
+
+
